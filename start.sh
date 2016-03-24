46f0e4858a588544b4e33ac66bc17977b5b8a140
Marcel Moldaschl <m.moldaschl@adito.de>
2016-03-24 18:40:03 +0100

    changed variable replacement

--- start.sh
+++ start.sh
@@ -1,19 +1,17 @@
 cp -f /a/config/ADITO4server.vmoptions /opt/ADITO4/bin/ADITO4server.vmoptions
 cp -f /a/config/serverconfig.xml /opt/ADITO4/config/serverconfig.xml
 
-
 cp -f /opt/ADITO4/webroot/webstart/config/template_client.jnlp /opt/ADITO4/webroot/webstart/config/client.jnlp
 
-#Replacement for Webstart
-sed -i s^http://host/webstart/config^"${WEBSTART_URL}"^g /opt/ADITO4/webroot/webstart/config/client.jnlp
+
+sed -i s^http://host^"${WEBSTART_URL:-"http://${WEBSTART_HOST}"}"^g /opt/ADITO4/webroot/webstart/config/client.jnlp
 sed -i s^host^"${WEBSTART_HOST}"^g /opt/ADITO4/webroot/webstart/config/client.jnlp
 sed -i s^7779^"${WEBSTART_PORT}"^g /opt/ADITO4/webroot/webstart/config/client.jnlp
-sed -i s^CLASSIC^"${WEBSTART_CONNECTION_TYPE}"^g /opt/ADITO4/webroot/webstart/config/client.jnlp
 
-#Replacement for serverconfig.xm.
-sed -i s/{WEBSTART_HOST}/"${WEBSTART_HOST}"/g /opt/ADITO4/config/serverconfig.xml
-sed -i s/{WEBSTART_URL}/"${WEBSTART_URL}"/g /opt/ADITO4/config/serverconfig.xml
-sed -i s/{WEBSTART_PORT}/"${WEBSTART_PORT}"/g /opt/ADITO4/config/serverconfig.xml
+#Default Connection Type for Adito 4.4 is CLASSIC; Default Connection Type for Adito 4.5 is NETTY
+sed -i s^CLASSIC^"${WEBSTART_CONNECTION_TYPE:-"NETTY"}"^g /opt/ADITO4/webroot/webstart/config/client.jnlp
+sed -i s^NETTY^"${WEBSTART_CONNECTION_TYPE:-"NETTY"}"^g /opt/ADITO4/webroot/webstart/config/client.jnlp
+
 sed -i s/{SRVCONF_DATABASE}/"${SRVCONF_DATABASE}"/g /opt/ADITO4/config/serverconfig.xml
 sed -i s/{SRVCONF_DATABASETYP}/"${SRVCONF_DATABASETYP}"/g /opt/ADITO4/config/serverconfig.xml
 sed -i s/{SRVCONF_HOST}/"${SRVCONF_HOST}"/g /opt/ADITO4/config/serverconfig.xml
@@ -26,5 +24,4 @@ sed -i s/{SRVCONF_SERVER_ID}/"${SRVCONF_SERVER_ID}"/g /opt/ADITO4/config/serverc
 sed -i s/{JVM_XMX}/"${JVM_XMX:-1024M}"/g /opt/ADITO4/bin/ADITO4server.vmoptions
 
 
-#Start the Server
 /opt/ADITO4/bin/ADITO4server
